90925e728e025609b47659740b87eb2cd4f23974
James G <stackz@protonmail.com>
2022-06-29 16:56:58 -0400

    created all models and category-routes with successful test

--- models/index.js
+++ models/index.js
@@ -5,8 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
+
+
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
 
